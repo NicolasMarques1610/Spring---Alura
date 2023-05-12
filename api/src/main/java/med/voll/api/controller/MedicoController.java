4592c46fcd427889031630e646a584fca2bcd08d
nicolas marques <nicolasmarques1205@gmail.com>
2023-05-12 15:56:24 -0300

    Aprendendo a montar os controllers, por enquanto só do médico

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json) { //requestBody para pegar do corpo da requisiçao
+        System.out.println(json);
+    }
+}
